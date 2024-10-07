aee97462217a5218158033073da1b2c4c4e02083
Thomas Thomsen <twt@outlook.dk>
2024-10-07 22:45:44 +0200

    animations

--- components/AudioTranscription.vue
+++ components/AudioTranscription.vue
@@ -19,7 +19,22 @@
           class="w-full bg-purple-900/50 backdrop-blur-md text-white py-2 px-4 rounded-md hover:bg-purple-900/40 transition duration-300 border border-white/30 shadow-lg"
           :disabled="loading"
         >
-          {{ loading ? 'Transcribing...' : 'Transcribe Audio' }}
+        <div class="w-full flex justify-center items-center h-6 overflow-hidden">
+          <Transition name="slide-up">
+            <span v-if="loading">
+              <div class="flex" role="status">
+                  <svg aria-hidden="true" class="w-6 h-6 text-purple-200 animate-spin fill-purple-400" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                  </svg>
+                  <Transition name="slide-up">
+                  <p v-show="estimatedProcessingTime" class="ml-3 text-white">{{ estimatedProcessingTime }}</p>
+                  </Transition>
+              </div>
+            </span>
+            <span v-else>Transcribe</span>
+          </Transition>
+        </div>
         </button>
       </div>
       
@@ -33,7 +48,7 @@
       </div> -->
       
 
-      <div v-if="loading" class="mb-4 flex justify-center items-center flex-col">
+<!--       <div v-if="loading" class="mb-4 flex justify-center items-center flex-col">
         
         <p class="text-white mb-4">{{ estimatedProcessingTime }}</p>
         <div class="flex" role="status">
@@ -44,7 +59,7 @@
             <p class="ml-3 text-purple-600">Transcribing...</p>
         </div>
 
-      </div>
+      </div> -->
 
       <div v-if="error" class="mb-4 p-4 bg-red-100 border border-red-400 text-red-700 rounded-md">
         {{ error }}
@@ -88,7 +103,7 @@ const estimatedProcessingTime = computed(() => {
     const estimatedSeconds = Math.round(duration.value * (84 / 76))
     const minutes = Math.floor(estimatedSeconds / 60)
     const seconds = estimatedSeconds % 60
-    return `Estimated time: ${minutes} minute${minutes !== 1 ? 's' : ''} and ${seconds} second${seconds !== 1 ? 's' : ''}`
+    return `ETA: ${minutes} min.${minutes !== 1 ? 's' : ''} and ${seconds} second${seconds !== 1 ? 's' : ''}`
   }
   return ''
 })
@@ -279,3 +294,25 @@ const elapsedTime = ref(0)
 const processingStartTime = ref(0)
 
 </script>
+
+<style>
+
+span {
+  position: absolute;
+}
+
+.slide-up-enter-active,
+.slide-up-leave-active {
+  transition: all 0.25s ease-out;
+}
+
+.slide-up-enter-from {
+  opacity: 0;
+  transform: translateY(0.7rem);
+}
+
+.slide-up-leave-to {
+  opacity: 0;
+  transform: translateY(-0.7rem);
+}
+</style>
