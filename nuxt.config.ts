c44e8dbb1c904d946b5faab384f16e07ebd9d846
Thomas Thomsen <twt@outlook.dk>
2024-10-11 23:03:11 +0200

    off thread

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['nuxt-workers', '@nuxtjs/tailwindcss', '@vueuse/motion/nuxt', '@nuxt/fonts'],
+  vite: {
+    optimizeDeps: {
+      include: ['onnxruntime-web']
+    }
+  },
   runtimeConfig: {
     domain: process.env.DOMAIN,
     path_to_store_temp_files: process.env.PATH_TO_STORE_TEMP_FILES,
