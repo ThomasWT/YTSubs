703fe6a00853b8aa41f8985d7dea7a85fe1687ff
Thomas <ThomasWT@users.noreply.github.com>
2024-10-15 12:37:45 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -14,12 +14,12 @@ useServerSeoMeta({
   title: 'YTSubs',
   description: 'Extract subtitles from YouTube videos using Client-side AI.',
   ogImage: 'https://ytsubs.thomaswt.com/ytogimage.jpg',
-  ogTitle: 'AI-Powered YouTube Subtitle Extractor',
+  ogTitle: 'AI-Powered YouTube Subtitle generator',
   ogDescription: 'Transform YouTube video content into subtitles',
   twitterCard: 'summary_large_image',
   twitterSite: '@eothica',
   robots: 'index, follow',
-  keywords: 'YouTube subtitles, video captions, AI subtitle extraction, YouTube transcription'
+  keywords: 'YouTube subtitles, video captions, AI subtitle generator, YouTube transcription'
 })
 
 // You can also use useHead for additional customization if needed
